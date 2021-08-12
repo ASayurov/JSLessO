1fa025071680e5f4d24818c20a7c1405a6151476
administrator <enniqx@gmail.com>
2021-08-12 13:57:07 +0300

    day 2

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,9 +20,53 @@ const personalMovieDB = {
     privat: false
 };
 
-const film = prompt("Один из последних просмотренных фильмов?", ""),
-      points = +prompt("На сколько оцение его?", "");
 
-personalMovieDB.movies[film] = points;
 
-console.log(personalMovieDB);
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов?", ""),
+            b = +prompt("На сколько оцение его?", "");
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done')
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено мало")
+    } else if (personalMovieDB >= 10 && personalMovieDB <= 30) {
+        console.log('Норм посомтрел')
+    } else if (personalMovieDB >= 30) {
+        console.log('Посмотрел много')
+    } else {
+        console.log('Ошибка')
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log('Информация приватна')
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?`)
+    }
+}
+
+writeYourGenres();
+
+
---
+++ test.js
@@ -0,0 +1,20 @@
+// "use strict";
+//
+// function  showFirstMessage(text) {
+//     console.log(text);
+//     let num = 20;
+// }
+//
+// showFirstMessage('hi');
+//
+//
+// function calc(a, b) {
+//     return (a + b);
+// }
+//
+// console.log(calc(4, 3));
+//
+// const calcs = (a, b) => a + b;
+//
+// const logg = "hello world";
+// console.log(logg.slice(0, 5));
